26fbe242fc7bc94f1b84854a25dca911e46e7ace
Alexandre Angrignon <alex040892@gmail.com>
2024-08-02 18:42:31 +1200

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useEffect } from 'react';
+import Banner from './components/banner';
 import Header from './components/header';
 import './styles/App.scss';
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
+
   return (
     <div className='App'>
       <Header />
+      <Banner />
     </div>
   );
 }
